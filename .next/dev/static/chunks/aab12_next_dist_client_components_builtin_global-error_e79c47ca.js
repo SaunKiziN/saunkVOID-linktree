68c4b5adcc11ac0c41fb87882b9bf78fd6c86870
S a u n K <46392328+SaunKiziN@users.noreply.github.com>
2026-07-03 10:55:10 -0300

    chore: stop tracking generated files

--- .next/dev/static/chunks/aab12_next_dist_client_components_builtin_global-error_e79c47ca.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/aab12_next_dist_efcb8e1d._.js"
-],
-    source: "dynamic"
-});
